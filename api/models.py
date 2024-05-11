40b36c6de39bf55bcfd9ad56036d833d3fa69712
kuriken121227 <kuri12ken27@gmail.com>
2024-05-11 15:40:29 +0900

    アカウント登録時の例外処理・パスワードの表示切替イメージ追加

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import IntegrityError, models
+from django.forms import ValidationError
 from django.utils import timezone
 
 # Create your models here.
@@ -6,7 +7,7 @@ from django.utils import timezone
 # ユーザー情報
 class User(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True, db_comment='ユーザ識別ID')  # Field name made lowercase.
-    name = models.CharField(max_length=255, db_comment='ユーザー名')
+    name = models.CharField(max_length=255, db_comment='ユーザー名', default='')
     loginid = models.CharField(db_column='loginID', unique=True, max_length=255, db_comment='ログインID')  # Field name made lowercase.
     passwd = models.CharField(max_length=255, db_comment='ログインパスワード')
     lastlogin = models.DateTimeField(db_column='lastLogin', db_comment='最終ログイン', auto_now=True)  # Field name made lowercase.
@@ -18,3 +19,16 @@ class User(models.Model):
         managed = False
         db_table = 'user'
         db_table_comment = 'ユーザマスタ'
+
+    def check_valid(self):
+        #IDとパスワードが同じでないことの確認
+        if self.loginid == self.passwd:
+            raise ValidationError('IDとパスワードは同じものを設定することはできません')
+            
+    def save(self):
+        try:
+            self.check_valid()
+        except ValidationError as e:
+            raise e
+        else:
+            super().save()
--- api/views.py
+++ api/views.py
@@ -1,14 +1,25 @@
+from django.forms import ValidationError
 from django.shortcuts import render
 from .models import User
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils import timezone
+from django.db import IntegrityError
 import json
 
 # Create your views here.
+# アカウント登録の処理
 def regist_user(request):
-    name = request.POST['name']
-    id = request.POST['id']
-    password = request.POST['password']
-    user = User(name = name, loginid = id, passwd = password, confid = 0)
-    user.save()
-    return HttpResponseRedirect('/accounts/rgst_account')
+    try:
+        name = request.POST['name']
+        id = request.POST['id']
+        password = request.POST['password']
+        user = User(name = name, loginid = id, passwd = password, confid = 0)
+        user.save()
+    except IntegrityError as e:
+        duplicate_error = 'このIDは既に利用されています。'
+        return render(request, 'accounts/rgst.html', {"duplicate_error": duplicate_error}, status=400)
+    except ValidationError as e:
+        same_error = e.message
+        return render(request, 'accounts/rgst.html', {"same_error": same_error}, status=400)
+    else:
+        return HttpResponseRedirect('/accounts/rgst_account')
