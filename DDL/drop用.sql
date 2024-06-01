ca63a9d659defdba3766fba56dc921b96b6a9489
kuriken121227 <kuri12ken27@gmail.com>
2024-06-01 22:19:38 +0900

    移してます

--- DDL/drop用.sql
+++
@@ -1,7 +0,0 @@
-drop table friend;
-drop table friendrequest;
-drop table chat;
-drop table roominfo;
-drop table chatroom;
-drop table conf;
-drop table user;
--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'api'
-
-urlpatterns = [
-    path(r'regist_user', views.regist_user, name='regist_user')
-]
