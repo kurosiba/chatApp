581cd55427fa872b78570c2703f5fde0ad4a0248
kuriken121227 <kuri12ken27@gmail.com>
2024-06-18 22:43:32 +0900

    ログインAPIの作成

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path(r'regist_user', views.regist_user, name='regist_user')
+    path(r'regist_user', views.regist_user, name='regist_user'),
+    path(r'check_login', views.check_login, name='check_login')
+
 ]
